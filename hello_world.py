e3329f007c5ec9884dd6e1576f8e069078c82279
Jay Godse <145635435+jgodsepar@users.noreply.github.com>
2026-01-19 17:28:25 -0500

    Update license terms in hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 # hello_world.py
 
-# hello_world is freely distributable under the terms of O'saay License.
+# hello_world is distributable under the terms of O'Saasy License.
 # See LICENSE.txt at the top of the repository or the license template at
 # http://www.osaasy.dev
 
