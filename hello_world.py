746d711b26cf63be6b5e5f4f1dec9115de3e43cf
Jay Godse <145635435+jgodsepar@users.noreply.github.com>
2026-01-19 13:09:12 -0500

    Add hello_world.py to print 'Hello World'

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+# hello_world.py
+
+# hello_world is freely distributable under the terms of O'saay License.
+# See LICENSE.txt at the top of the repository or the license template at
+# http://www.osaasy.dev
+
+print ("Hello World") 
